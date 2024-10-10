fe68f379dc7b3d12cf815a53719c6378fe4fcd60
Md Alamin <68142991+alamin516@users.noreply.github.com>
2024-10-10 12:35:13 +0600

    Update cookie.js

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -2,7 +2,7 @@ const setAccessTokenCookie = (res, accessToken, time = "15") => {
   res.cookie("access_token", accessToken, {
     maxAge: time * 60 * 60 * 1000,
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
     sameSite: "none",
   });
 };
@@ -11,7 +11,7 @@ const setRefreshTokenCookie = (res, refreshToken, time = "7") => {
   res.cookie("refresh_token", refreshToken, {
     maxAge: time * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
     sameSite: "none",
   });
 };
