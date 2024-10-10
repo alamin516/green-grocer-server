4fb09551c72b30402c910859925a9847f8742c8d
Md Alamin <alaminduc516@gmail.com>
2024-10-10 10:42:09 +0600

    :zap: update cookie

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -2,8 +2,8 @@ const setAccessTokenCookie = (res, accessToken, time = "15") => {
   res.cookie("access_token", accessToken, {
     maxAge: time * 60 * 60 * 1000,
     httpOnly: true,
-    secure: true,
-    sameSite: "lax",
+    sameSite: "None",
+    secure: process.env.NODE_ENV === "production",
   });
 };
 
@@ -11,8 +11,8 @@ const setRefreshTokenCookie = (res, refreshToken, time = "7") => {
   res.cookie("refresh_token", refreshToken, {
     maxAge: time * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: true,
-    sameSite: "lax",
+    sameSite: "None",
+    secure: process.env.NODE_ENV === "production",
   });
 };
 
