fca98b3e485667df1bfcaa839fc219ee82051c94
Md Alamin <68142991+alamin516@users.noreply.github.com>
2024-10-12 11:25:36 +0600

    Update cookie.js

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 
 const setAccessTokenCookie = (res, accessToken) => {
   res.cookie("access_token", accessToken, {
-    domain: "green-grocer-mart.web.app",
-    path: "/",
     maxAge: 15 * 60 * 60 * 1000,
     httpOnly: true,
     secure: true, 
@@ -13,8 +11,6 @@ const setAccessTokenCookie = (res, accessToken) => {
 
 const setRefreshTokenCookie = (res, refreshToken) => {
   res.cookie("refresh_token", refreshToken, {
-    domain: "green-grocer-mart.web.app",
-    path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: true,
