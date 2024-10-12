fcf146120868a904b353696e16e8a150a666f6b1
Md Alamin <68142991+alamin516@users.noreply.github.com>
2024-10-12 10:46:19 +0600

    Update cookie.js

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,26 +1,23 @@
 require("dotenv").config();
-
-const setAccessTokenCookie = (res, accessToken, time = process.env.ACCESS_TOKEN_EXPIRE || 15) => {
-  time = String(time);
+const setAccessTokenCookie = (res, accessToken, time = "15") => {
   res.cookie("access_token", accessToken, {
-    expires: new Date(Date.now() + time * 60 * 60 * 1000), 
+    expires: new Date(Date.now() + time * 60 * 60 *  1000),
     maxAge: time * 60 * 60 * 1000,
     httpOnly: true,
-    secure: true, 
+    secure: true,
     sameSite: "lax",
-    domain: "green-grocer-mart.web.app",
+    domain: "green-grocer-mart.web.app"
   });
 };
 
-const setRefreshTokenCookie = (res, refreshToken, time = process.env.REFRESH_TOKEN_EXPIRE || 7) => {
-  time = String(time);
+const setRefreshTokenCookie = (res, refreshToken, time = "7") => {
   res.cookie("refresh_token", refreshToken, {
-    expires: new Date(Date.now() + time * 24 * 60 * 60 * 1000),
+    expires: new Date(Date.now() + time * 24 * 60 * 60 *  1000),
     maxAge: time * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: true,
     sameSite: "lax",
-    domain: "green-grocer-mart.web.app",
+    domain: "green-grocer-mart.web.app"
   });
 };
 
