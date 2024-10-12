fd6d5374d4528c00ce16445d668bba0ccf5f5d2f
Md Alamin <68142991+alamin516@users.noreply.github.com>
2024-10-12 11:05:12 +0600

    Update cookie.js

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -2,23 +2,23 @@ require("dotenv").config();
 
 const setAccessTokenCookie = (res, accessToken, time = "15") => {
   res.cookie("access_token", accessToken, {
-    expires: new Date(Date.now() + time * 60 * 60 * 1000), // time in hours
-    maxAge: time * 60 * 60 * 1000, // maxAge in milliseconds
-    httpOnly: true, // Accessible only by the web server (not client-side JavaScript)
-    secure: true, // Cookie is only sent over HTTPS
-    sameSite: "lax", // Helps prevent CSRF attacks
-    domain: "green-grocer-mart.web.app", // Correct domain format
+    domain: "green-grocer-mart.web.app",
+    path: "/",
+    maxAge: time * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: true, 
+    sameSite: "none",
   });
 };
 
 const setRefreshTokenCookie = (res, refreshToken, time = "7") => {
   res.cookie("refresh_token", refreshToken, {
-    expires: new Date(Date.now() + time * 24 * 60 * 60 * 1000), // time in days
-    maxAge: time * 24 * 60 * 60 * 1000, // maxAge in milliseconds
-    httpOnly: true, // Accessible only by the web server (not client-side JavaScript)
-    secure: true, // Cookie is only sent over HTTPS
-    sameSite: "lax", // Helps prevent CSRF attacks
-    domain: "green-grocer-mart.web.app", // Correct domain format
+    domain: "green-grocer-mart.web.app",
+    path: "/",
+    maxAge: time * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
   });
 };
 
